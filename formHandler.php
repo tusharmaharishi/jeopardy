09b2352ae9a06489f4781dfd90d57aa8b5cf4c0a
Tushar Maharishi <tm5gf@virginia.edu>
2017-02-21 21:55:30 -0500

    properly confirmed data from table and printed to a textfile, does not fix repeat submission problem

--- formHandler.php
+++ formHandler.php
@@ -9,7 +9,9 @@
   <p>
     The following table lists all parameter names and their values that were submitted from your form.
   </p>
-
+  <?php
+     $GLOBALS['information'] = array();
+     ?>
   <table cellSpacing=1 cellPadding=1 width="75%" border=1 bgColor="lavender">
     <tr bgcolor="#FFFFFF">
       <td align="center"><strong>Parameter</strong></td>
@@ -19,8 +21,10 @@
       <td width="20%">Multiple Choice - Question</td>
       <td>
         <?php
-        if(isset($_POST['multipleChoiceQuestion']))
-          echo $_POST['multipleChoiceQuestion'];
+        if(isset($_POST['multipleChoiceQuestion'])) {
+           echo $_POST['multipleChoiceQuestion'];
+	   $GLOBALS['information'][] = $_POST['multipleChoiceQuestion']; // same as array.append()
+	   }
         ?>
       </td>      
     </tr>
@@ -29,9 +33,10 @@
       <td>
         <?php 
         if(isset($_POST['multipleChoiceAnswer1'])) {
-          echo $_POST['multipleChoiceAnswer1'];
-          if($_POST['mc_correct'] == "A")
-            echo " (correct)";
+           echo $_POST['multipleChoiceAnswer1'];
+	   $GLOBALS['information'][] = $_POST['multipleChoiceAnswer1'];
+           if($_POST['mc_correct'] == "A")
+           echo " (correct)";
         }
         ?> 
       </td>      
@@ -42,6 +47,7 @@
         <?php 
         if(isset($_POST['multipleChoiceAnswer2'])) {
           echo $_POST['multipleChoiceAnswer2'];
+	   $GLOBALS['information'][] = $_POST['multipleChoiceAnswer2'];
           if($_POST['mc_correct'] == "B")
             echo " (correct)";
         }
@@ -53,6 +59,7 @@
       <td>
         <?php 
         if(isset($_POST['multipleChoiceAnswer3'])) {
+	   $GLOBALS['information'][] = $_POST['multipleChoiceAnswer3'];
           echo $_POST['multipleChoiceAnswer3'];
           if($_POST['mc_correct'] == "C")
             echo " (correct)";
@@ -65,6 +72,7 @@
       <td>
         <?php 
         if(isset($_POST['multipleChoiceAnswer4'])) {
+           $GLOBALS['information'][] = $_POST['multipleChoiceAnswer4'];
           echo $_POST['multipleChoiceAnswer4'];
           if($_POST['mc_correct'] == "D")
             echo " (correct)";
@@ -76,8 +84,10 @@
       <td width="20%">True or False - Question</td>
       <td>
         <?php 
-        if(isset($_POST['trueOrFalseQuestion']))
-          echo $_POST['trueOrFalseQuestion'];
+        if(isset($_POST['trueOrFalseQuestion'])) {
+           echo $_POST['trueOrFalseQuestion'];
+	   $GLOBALS['information'][] = $_POST['trueOrFalseQuestion'];
+	   }
         ?>
       </td>      
     </tr>
@@ -85,14 +95,19 @@
       <td width="20%">True or False - Answer</td> 
       <td>
         <?php 
-	       //$variable = 5;
-	       //file_put_contents("data.txt","FROM TRUE OR FALSE ANSWER", FILE_APPEND | LOCK_EX);
-        if(isset($_POST['trueOrFalseAnswer'])) {
-          $ans = $_POST['trueOrFalseAnswer'];
-          if($ans == 'true')
-            echo "True (correct)";
-          else
-            echo "True";
+	   //$GLOBALS['information'][] = 5;
+           //$GLOBALS['information'][] = 4;
+           //$GLOBALS['information'][] = 3;
+	   if(isset($_POST['trueOrFalseAnswer'])) {
+           $ans = $_POST['trueOrFalseAnswer'];
+           if($ans == 'true') {
+           echo "True (correct)";
+	   $GLOBALS['information'][] = "True (correct)";  
+	   }
+           else {
+           echo "True";
+	   $GLOBALS['information'][] = "True";
+	   }
         }
         ?>
       </td>      
@@ -103,10 +118,14 @@
         <?php 
         if(isset($_POST['trueOrFalseAnswer'])) {
           $ans = $_POST['trueOrFalseAnswer'];
-          if($ans == 'true')
+          if($ans == 'true') {
             echo "False";
-          else
+           $GLOBALS['information'][] = "False";
+	   }
+          else {
             echo "False (correct)";
+           $GLOBALS['information'][] = "False (correct)";
+	   }
         }
         ?>
       </td>      
@@ -115,8 +134,10 @@
       <td width="20%">Short Answer - Question</td>
       <td>
         <?php 
-        if(isset($_POST['shortAnswerQuestion']))
+        if(isset($_POST['shortAnswerQuestion'])) {
           echo $_POST['shortAnswerQuestion'];
+	   $GLOBALS['information'][] = $_POST['shortAnswerQuestion'];
+	   }
         ?>
       </td>      
     </tr>
@@ -125,21 +146,18 @@
       <td>
         <?php
         if(isset($_POST['shortAnswerAnswer'])) {
-         echo $_POST['shortAnswerAnswer'];
-         echo " (correct)";
+           echo $_POST['shortAnswerAnswer'];
+           echo " (correct)";
+	   $GLOBALS['information'][] = $_POST['trueOrFalseQuestion']." (correct)";
         }
        ?>
-     </td>      
-   </tr>
-   <!--<tr>
-    <td width="20%">submit</td>
-    <td><?PHP ECHO $_POST['submit']?></td>      
-  </tr>-->    
-</table>
+      </td>      
+    </tr>
+  </table>
 </br>
 <button onclick="history.go(-1);">Back </button>
 <form method='post' action="formHandler.php" name="functionPrint">
-  <input type="hidden" value="print" name="actionprint" />
+  <input type="hidden" value="<?php echo implode(',',$GLOBALS['information']);?>" name="actionprint" />
   <button type="button" onclick="window.document.functionPrint.submit()">Confirm and Print to File</button>
 </form>
 <!-- <script type='text/javascript'>
@@ -163,13 +181,33 @@
 </html> 
 
 <?php
- 
-if(isset($_POST['actionprint'])) {
-    printToFile();
-  }
 
+  function checkActionPrint() {
+   foreach($GLOBALS['information'] as &$curr)
+   echo $curr."<br>";
+   }
+
+   if(isset($_POST['actionprint']))
+   {
+   /*
+   echo "FOUND Ids<br>";
+   $IdArray = explode(',',$_POST['actionprint']);
+   print_r($IdArray);
+   */
+   printToFile();
+   }
+   
   function printToFile() {
-    $txt = 'HELLOOOOO';
-    file_put_contents('data.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
-  }
+   $submission_data_arr = explode(',',$_POST['actionprint']);
+   foreach($submission_data_arr as &$curr) {
+   file_put_contents('data.txt', $curr.PHP_EOL, FILE_APPEND | LOCK_EX);
+   }
+   }
+   /*
+   $txt = '';
+   if(isset($_POST['trueOrFalseQuestion'])) {
+   $txt = 'HELLOOOOO';
+   }
+   file_put_contents('data.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
+   */
 ?>

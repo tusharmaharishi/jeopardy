682b1ba0374efe27f3d067adbd41669ec3fdf3db
Tushar Maharishi <tm5gf@virginia.edu>
2017-02-26 18:47:29 -0500

    made better variable names

--- formHandler.php
+++ formHandler.php
@@ -10,7 +10,7 @@
     The following table lists all parameter names and their values that were submitted from your form.
   </p>
   <?php
-     $GLOBALS['information'] = array();
+     $GLOBALS['responses'] = array();
      ?>
   <table cellSpacing=1 cellPadding=1 width="75%" border=1 bgColor="lavender">
     <tr bgcolor="#FFFFFF">
@@ -23,7 +23,7 @@
         <?php
         if(isset($_POST['multipleChoiceQuestion'])) {
            echo $_POST['multipleChoiceQuestion'];
-	         $GLOBALS['information'][] = $_POST['multipleChoiceQuestion']; // same as array.append()
+	         $GLOBALS['responses'][] = $_POST['multipleChoiceQuestion']; // same as array.append()
 	       }
         ?>
       </td>      
@@ -34,7 +34,7 @@
         <?php 
         if(isset($_POST['multipleChoiceAnswer1'])) {
            echo $_POST['multipleChoiceAnswer1'];
-	         $GLOBALS['information'][] = $_POST['multipleChoiceAnswer1'];
+	         $GLOBALS['responses'][] = $_POST['multipleChoiceAnswer1'];
            if($_POST['mc_correct'] == "A")
            echo " (correct)";
         }
@@ -47,7 +47,7 @@
         <?php 
         if(isset($_POST['multipleChoiceAnswer2'])) {
           echo $_POST['multipleChoiceAnswer2'];
-	   $GLOBALS['information'][] = $_POST['multipleChoiceAnswer2'];
+	   $GLOBALS['responses'][] = $_POST['multipleChoiceAnswer2'];
           if($_POST['mc_correct'] == "B")
             echo " (correct)";
         }
@@ -59,7 +59,7 @@
       <td>
         <?php 
         if(isset($_POST['multipleChoiceAnswer3'])) {
-	   $GLOBALS['information'][] = $_POST['multipleChoiceAnswer3'];
+	   $GLOBALS['responses'][] = $_POST['multipleChoiceAnswer3'];
           echo $_POST['multipleChoiceAnswer3'];
           if($_POST['mc_correct'] == "C")
             echo " (correct)";
@@ -72,7 +72,7 @@
       <td>
         <?php 
         if(isset($_POST['multipleChoiceAnswer4'])) {
-           $GLOBALS['information'][] = $_POST['multipleChoiceAnswer4'];
+           $GLOBALS['responses'][] = $_POST['multipleChoiceAnswer4'];
           echo $_POST['multipleChoiceAnswer4'];
           if($_POST['mc_correct'] == "D")
             echo " (correct)";
@@ -86,7 +86,7 @@
         <?php 
         if(isset($_POST['trueOrFalseQuestion'])) {
            echo $_POST['trueOrFalseQuestion'];
-	   $GLOBALS['information'][] = $_POST['trueOrFalseQuestion'];
+	   $GLOBALS['responses'][] = $_POST['trueOrFalseQuestion'];
 	   }
         ?>
       </td>      
@@ -95,18 +95,18 @@
       <td width="20%">True or False - Answer</td> 
       <td>
         <?php 
-	   //$GLOBALS['information'][] = 5;
-           //$GLOBALS['information'][] = 4;
-           //$GLOBALS['information'][] = 3;
+	   //$GLOBALS['responses'][] = 5;
+           //$GLOBALS['responses'][] = 4;
+           //$GLOBALS['responses'][] = 3;
 	   if(isset($_POST['trueOrFalseAnswer'])) {
            $ans = $_POST['trueOrFalseAnswer'];
            if($ans == 'true') {
            echo "True (correct)";
-	   $GLOBALS['information'][] = "True (correct)";  
+	   $GLOBALS['responses'][] = "True (correct)";  
 	   }
            else {
            echo "True";
-	   $GLOBALS['information'][] = "True";
+	   $GLOBALS['responses'][] = "True";
 	   }
         }
         ?>
@@ -120,11 +120,11 @@
           $ans = $_POST['trueOrFalseAnswer'];
           if($ans == 'true') {
             echo "False";
-           $GLOBALS['information'][] = "False";
+           $GLOBALS['responses'][] = "False";
 	   }
           else {
             echo "False (correct)";
-           $GLOBALS['information'][] = "False (correct)";
+           $GLOBALS['responses'][] = "False (correct)";
 	   }
         }
         ?>
@@ -136,7 +136,7 @@
         <?php 
         if(isset($_POST['shortAnswerQuestion'])) {
           echo $_POST['shortAnswerQuestion'];
-	   $GLOBALS['information'][] = $_POST['shortAnswerQuestion'];
+	   $GLOBALS['responses'][] = $_POST['shortAnswerQuestion'];
 	   }
         ?>
       </td>      
@@ -148,17 +148,17 @@
         if(isset($_POST['shortAnswerAnswer'])) {
            echo $_POST['shortAnswerAnswer'];
            echo " (correct)";
-	   $GLOBALS['information'][] = $_POST['trueOrFalseQuestion']." (correct)";
+	   $GLOBALS['responses'][] = $_POST['trueOrFalseQuestion']." (correct)";
         }
        ?>
       </td>      
     </tr>
   </table>
 </br>
-<form method='post' action="formHandler.php" name="functionPrint">
+<form method='post' action="formHandler.php" name="confirmation">
   <button onclick="history.go(-1);">Back </button>
-  <input type="hidden" value="<?php echo implode(',',$GLOBALS['information']);?>" name="actionprint" />
-  <button type="button" onclick="window.document.functionPrint.submit()">Confirm and Print to File</button>
+  <input type="hidden" value="<?php echo implode(',',$GLOBALS['responses']);?>" name="actionprint" />
+  <button type="button" onclick="window.document.confirmation.submit()">Confirm and Print to File</button>
 </form>
 <!-- <script type='text/javascript'>
     
@@ -183,7 +183,7 @@
 <?php
 
   function checkActionPrint() {
-   foreach($GLOBALS['information'] as &$curr)
+   foreach($GLOBALS['responses'] as &$curr)
    echo $curr."<br>";
    }
 

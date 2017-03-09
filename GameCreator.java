421e1dd083d0b985d6d75e31f161d656697fc666
Tushar Maharishi <tm5gf@virginia.edu>
2017-03-09 16:12:31 -0500

    Finished doPost in servlet to create board (without CSS)

--- GameCreator.java
+++ GameCreator.java
@@ -1,8 +1,13 @@
 package cs4640;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL; // URL
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner; // Scanner
 
 import javax.servlet.ServletException;
@@ -47,7 +52,8 @@ public class GameCreator extends HttpServlet {
 		out.println("	</head>");
 		out.println("	<body>");
 		out.println("		<h1 align='center'>Position Selector</h1>");
-		out.println("		<form action='GameCreator' method='post'>"); // form contains the table
+		out.println("		<h3 align='center'>Tushar Maharishi (tm5gf) & Hannah Pham (htp3xj)</h3>");
+		out.println("		<form action='GameCreator' method='post' align='center'>"); // form contains the table
 		out.println("			<table border='1' cellspacing='2' cellpadding='5' width='75%' align='center'>");
 
 		// initialize variables to help with program
@@ -64,21 +70,28 @@ public class GameCreator extends HttpServlet {
 				out.println("<tr>");
 				currLine = sc.nextLine(); // drop the "____Question" title in data.txt
 
-				// new column for question/answer
+				// new column for question/answer, hidden input for question 
 				out.println("	<td>");
-				out.println("		<b>Question: </b>" + sc.nextLine() + "<br>");
+				currLine = sc.nextLine();
+				out.println("		<b>Question: </b>" + currLine + "<br>");
+				String questionName = "question" + questionCount;
+				out.println("		<input type='hidden' name='" + questionName + "' value='" + currLine + "'>");
+				
+				// assemble the answers to the question by building them into a single line, hidden input for answers
 				currLine = sc.nextLine(); // drop the "____Answer" title in data.txt
-
-				// assemble the answers to the question by building them into a single line
 				String answersLine = "";
+				String answersLineSemicolon = "";
 				while (!currLine.equals("") && sc.hasNextLine()) {
 					currLine = sc.nextLine();
 					answersLine += currLine + ", ";
+					answersLineSemicolon += currLine + ";";
 					if (sc.hasNextLine()) // in case we have reached the end of the text file
-						currLine = sc.nextLine();
+						currLine = sc.nextLine(); // drop the "____Answer" title in data.txt
 				}
-				answersLine = answersLine.substring(0, answersLine.length() - 2);
+				answersLine = answersLine.substring(0, answersLine.length() - 2); // -2 to get rid of the final comma
 				out.println("		<b>Answer(s): </b>" + answersLine);
+				String answersName = "answers" + questionCount;
+				out.println("		<input type='hidden' name='" + answersName + "' value='" + answersLineSemicolon.substring(0, answersLineSemicolon.length()-1) + "'>");
 
 				// new column for row position
 				out.println("	</td>");
@@ -100,7 +113,10 @@ public class GameCreator extends HttpServlet {
 
 		// finish the HTML page
 		out.println("			</table>");
-		out.println("			<input type='submit' value='createGame'>");
+		out.println("			<input type='hidden' name='numQuestions' value='" + questionCount + "'>");
+		out.println("			<br>");
+		out.println("			<input type=\"button\" onclick=\"location.href='http://plato.cs.virginia.edu/~tm5gf/CS4640/jeopardy/project3/jeopardy.html';\" value=\"Make another Q/A\" style='font-size: 16px;'/>");
+		out.println("			<input type='submit' value='Create Game' style='font-size: 16px;'>");
 		out.println("		</form>");
 		out.println("	</body>");
 		out.println("</html>");
@@ -116,17 +132,87 @@ public class GameCreator extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		// whatever is received will be produced into an html document
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
+		// initialize printers
+		String filename = "gameCreationData.txt";
+		File file = new File(filename);
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter outfile = new BufferedWriter(fw);
+		// System.out.println(file.getAbsolutePath()); // check where the file ended up... fix this later
+		
+		// store posted data into lists
+		ArrayList<String> questionList = new ArrayList<String>();
+		ArrayList<String> answersList = new ArrayList<String>();
+		ArrayList<Integer> rowList = new ArrayList<Integer>();
+		ArrayList<Integer> colList = new ArrayList<Integer>();
+		ArrayList<Integer> scoreList = new ArrayList<Integer>();
+		int questionCount = Integer.parseInt(request.getParameter("numQuestions"));
+		for(int i = 0; i < questionCount; i++) {
+
+			// retrieving the information from the post request
+			questionList.add(request.getParameter("question" + i));
+			answersList.add(request.getParameter("answers" + i));
+			rowList.add(Integer.parseInt(request.getParameter("row" + i))); // remember to make sure that input is numbers!!!
+			colList.add(Integer.parseInt(request.getParameter("col" + i))); // remember to make sure that input is numbers!!!
+			scoreList.add(Integer.parseInt(request.getParameter("score" + i))); // remember to make sure that input is numbers!!!
+			
+			// printing the information to the text file, separated by semicolons
+			outfile.write(questionList.get(i) + ";" + answersList.get(i) + ";" + rowList.get(i) + ";" + colList.get(i) + ";" + scoreList.get(i));
+			outfile.newLine();
+		}
+
+		// properly close BufferedWriter as printing is finished
+		outfile.flush();
+		outfile.close();
+		
+		// initialize board with -1 values
+		int tableMaxRow = Collections.max(rowList);
+		int tableMaxCol = Collections.max(colList);
+		int[][] board = new int[tableMaxRow][tableMaxCol];
+		for(int x = 0; x < board.length; x++) 
+			for(int y = 0; y < board[0].length; y++)
+				board[x][y] = -1;
+		
+		// adding the scores to the rows/cols specified from form input
+		for(int i = 0; i < questionCount; i++) {
+			board[rowList.get(i)-1][colList.get(i)-1] = scoreList.get(i);
+		}
+		
+		// begin HTML page
 		out.println("<html>");
 		out.println("	<head>");
 		out.println("		<title>Position Selector</title>");
 		out.println("	</head>");
 		out.println("	<body>");
-		out.println("	Hello world 1<br>");
-		out.println("Parameter: " + request.getParameter("score1") + "<br>");
-		out.println("	Hello world 2<br>");
+		out.println("		<table border='1' cellspacing='2' cellpadding='5' width='75%' align='center'>");
+
+		// start creating HTML table for game board
+		for(int x = 0; x < board.length; x++) {
+			
+			// create new row
+			out.println("<tr>");
+			for(int y = 0; y < board[0].length; y++) {
+
+				// create new column
+				out.println("<td>" + board[x][y] + "</td>");
+			}
+			out.println("</tr>");
+		}
+		out.println("		</table>");
+		out.println("		<br>");
+		out.println("		<div align='center'>");
+		out.println("			<button onclick='history.go(-1);' style='font-size: 16px;' align='center'> Back </button>");
+		out.println("		</div>");
+		
+//		out.println("	Hello world 1<br>");
+//		out.println("Parameter: " + request.getParameter("numQuestions") + "<br>");
+//		out.println("	Hello world 2<br>");
+		
+		
+		
 		out.println("	</body>");
 		out.println("</html>");
 	}
